a19298cdafb3c93293bf4a5317d5d64d8d8bab49
gritman <gritman@126.com>
2017-05-29 22:45:36 +0800

    日志查看钩子的调用顺序

--- src/app/life/life.component.ts
+++ src/app/life/life.component.ts
@@ -1,7 +1,10 @@
 import {
-  AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy,
+  AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges,
+  OnDestroy,
   OnInit, SimpleChanges
 } from '@angular/core';
+
+let logIndex = 1;
 
 @Component({
   selector: 'app-life',
@@ -11,30 +14,53 @@ import {
 export class LifeComponent implements
   OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy
 {
-  ngOnChanges(changes: SimpleChanges): void {
+  @Input()
+  name: string;
+
+  logIt(msg: string) {
+    // 注意,下面不是单引号',而是`
+    console.log(`#${logIndex++} ${msg}`);
   }
 
+  constructor() {
+    this.logIt('name 属性在 constructor 里的值是: ' + name);
+  }
+
+  // 当父组件修改子组件的属性时调用
+  ngOnChanges(changes: SimpleChanges): void {
+    const name = changes['name'].currentValue;
+    this.logIt('name 属性在 ngOnChanges 里的值是: ' + name);
+  }
+
+  // 如果组件的初始化需要用到父组件传来的值,那么这个初始化要写在ngOnInit里
+  // 因为组件的构造函数在父传值之前被调用
+  ngOnInit() {
+    this.logIt('ngOnInit');
+  }
+
+  // 每个变更检测周期被调用
   ngDoCheck(): void {
+    this.logIt('ngDoCheck');
   }
 
   ngAfterContentInit(): void {
+    this.logIt('ngAfterContentInit');
   }
 
   ngAfterContentChecked(): void {
+    this.logIt('ngAfterContentChecked');
   }
 
   ngAfterViewInit(): void {
+    this.logIt('ngAfterViewInit');
   }
 
   ngAfterViewChecked(): void {
+    this.logIt('ngAfterViewChecked');
   }
 
   ngOnDestroy(): void {
-  }
-
-  constructor() { }
-
-  ngOnInit() {
+    this.logIt('ngOnDestroy');
   }
 
 }

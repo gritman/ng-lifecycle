4337512167741d529671234051e5a1de57c7ac8b
gritman <gritman@126.com>
2017-05-29 22:18:43 +0800

    继承并实现所有钩子方法

--- src/app/life/life.component.ts
+++ src/app/life/life.component.ts
@@ -1,11 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy,
+  OnInit, SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-life',
   templateUrl: './life.component.html',
   styleUrls: ['./life.component.css']
 })
-export class LifeComponent implements OnInit {
+export class LifeComponent implements
+  OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy
+{
+  ngOnChanges(changes: SimpleChanges): void {
+  }
+
+  ngDoCheck(): void {
+  }
+
+  ngAfterContentInit(): void {
+  }
+
+  ngAfterContentChecked(): void {
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  ngAfterViewChecked(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
 
   constructor() { }
 

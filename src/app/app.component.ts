e71f1cd33fea7df0728ed8d5b4eca0c4a001b930
gritman <gritman@126.com>
2017-05-30 15:29:46 +0800

    在父组件的控制器中调用子组件的方法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Child2Component} from './child2/child2.component';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  title = 'Tom';
+export class AppComponent implements OnInit {
+
+  @ViewChild('child1') // 模板本地变量
+  child1: Child2Component; // 在父组件里,获得一个子组件的引用
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.child1.greeting('Tom'); // 通过模板变量的名字来使用子组件
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { LifeComponent } from './life/life.component';
 import { ChildComponent } from './child/child.component';
 import { Life2Component } from './life2/life2.component';
+import { Child2Component } from './child2/child2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LifeComponent,
     ChildComponent,
-    Life2Component
+    Life2Component,
+    Child2Component
   ],
   imports: [
     BrowserModule,
